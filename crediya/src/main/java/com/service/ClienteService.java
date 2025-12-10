1f0310bd3a22bc863fb7fc73aab90e2b7b611670
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-10 17:04:07 -0500

    feat: :bug: Mejorar la clase ClienteService con validaciones de entrada y correcciones de excepciones.

--- crediya/src/main/java/com/service/ClienteService.java
+++ crediya/src/main/java/com/service/ClienteService.java
@@ -1,26 +1,27 @@
 package com.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.datavault.ClienteDv;
 import com.datavault.jdbc.ClienteJdbcDv;
 import com.model.Cliente;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 public class ClienteService {
+
     private final ClienteDv clienteDv;
 
-    public ClienteService() throws Exception{
+    public ClienteService() throws SQLException {
         this.clienteDv = new ClienteJdbcDv();
     }
 
-    public Cliente  crearCliente(Cliente c) throws Exception{
+    public Cliente crearCliente(Cliente c) throws Exception {
         if (c.getNombre() == null || c.getNombre().isEmpty()) {
-            throw new Exception("El nombre no puede estar vacio");
+            throw new Exception("el nombre no puede estar vacio");
         }
-        if (c.getDocumento() == null || c.getDocumento().isEmpty())
-            throw new Exception("El documento no puede estar vacio"); {
-            
+        if (c.getDocumento() == null || c.getDocumento().isEmpty()) {
+            throw new Exception("el documento no puede estar vacio");
         }
         return clienteDv.save(c);
     }
@@ -33,13 +34,14 @@ public class ClienteService {
         return clienteDv.findAll();
     }
 
-     public void actualizarCliente(Cliente c) throws Exception {
+    public void actualizarCliente(Cliente c) throws Exception {
         if (c.getId() == null) {
             throw new Exception("el id del cliente es obligatorio");
         }
         clienteDv.update(c);
     }
-    public void eliminarCliente(int id) throws Exception{
-        clienteDv.findById(id);
+
+    public void eliminarCliente(int id) throws Exception {
+        clienteDv.deleteById(id);
     }
 }

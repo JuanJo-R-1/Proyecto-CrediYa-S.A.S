d825d1e545090552c9d0258818e8c7445d5aee41
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-10 17:41:37 -0500

    feat: :sparkles: Añadir la clase Pago con atributos, metodos y sobreescritura de toString, equals y hashCode.

--- crediya/src/main/java/com/model/Pago.java
+++ crediya/src/main/java/com/model/Pago.java
@@ -1,9 +1,97 @@
 package com.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Pago {
-    private Integer id, prestamoId;
-    private LocalDate fechaPago;
+
+    private Integer id;
+    private Integer idCliente;
     private double monto;
+    private LocalDate fechaPago;
+    private String metodoPago;
+    private String estado;
+
+    public Pago(Integer id, Integer idCliente, double monto, LocalDate fechaPago, String metodoPago, String estado) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.monto = monto;
+        this.fechaPago = fechaPago;
+        this.metodoPago = metodoPago;
+        this.estado = estado;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Pago{" +
+                "id=" + id +
+                ", idCliente=" + idCliente +
+                ", monto=" + monto +
+                ", fechaPago=" + fechaPago +
+                ", metodoPago='" + metodoPago + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pago)) return false;
+        Pago pago = (Pago) o;
+        return Objects.equals(id, pago.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+

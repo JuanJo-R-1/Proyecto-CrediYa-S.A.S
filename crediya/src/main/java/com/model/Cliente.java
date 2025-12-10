712dcdef22f19136ddc985f1163ebf1fd06c854a
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-10 14:54:01 -0500

    feat: :sparkles: Refactorizar la clase Cliente para mejorar la legibilidad y el mantenimiento separando las declaraciones de campos y agregando los metodos faltantes.

--- crediya/src/main/java/com/model/Cliente.java
+++ crediya/src/main/java/com/model/Cliente.java
@@ -1,6 +1,83 @@
 package com.model;
 
+import java.util.Objects;
+
 public class Cliente {
     private Integer id;
-    private String nombre, documento, correo, telefono;
+    private String nombre;
+    private String documento;
+    private String correo;
+    private String telefono;
+
+    public Cliente(Integer id, String nombre, String documento, String correo, String telefono ){
+        this.id = id;
+        this.nombre = nombre;
+        this.documento = documento;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getDocumento(){
+        return documento;
+    }
+
+    public void setDocumento(String documento){
+        this.documento = documento;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString(){
+        return "Cliente{" +
+                " id = " + id +
+                ", nombre = '" + nombre + '\'' +
+                ", documento = '" + documento + '\'' +
+                ", correo = '" + correo + '\'' +
+                ", telefono = '" + telefono + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

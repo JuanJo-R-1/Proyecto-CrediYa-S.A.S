78bc7b3f3d1ab68484797eea635e2a17bf393ee5
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-12-10 17:49:57 -0500

    feat: :sparkles: Definir la interfaz PagoDv con metodos para gestionar pagos.

--- crediya/src/main/java/com/datavault/PagoDv.java
+++ crediya/src/main/java/com/datavault/PagoDv.java
@@ -1,5 +1,15 @@
 package com.datavault;
 
-public class PagoDv {
-    
+import com.model.Pago;
+import java.util.List;
+import java.util.Optional;
+
+public interface PagoDv {
+
+    Pago save(Pago pago) throws Exception;
+    Optional<Pago> findById(int id) throws Exception;
+    List<Pago> findAll() throws Exception;
+    List<Pago> findByPrestamoId(int prestamoId) throws Exception;
+    void update(Pago pago) throws Exception;
+    void deleteById(int id) throws Exception;
 }

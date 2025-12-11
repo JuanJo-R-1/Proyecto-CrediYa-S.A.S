6d4ad21e0d731114aa5660f8a7efc91caa3c1995
JuanJo Rivero <riverocamargojuanjose@gmail.com>
2025-12-11 13:19:08 -0500

    feat: :sparkles: Se implementa la clase PrestamoDv y PrestamoJdbcDv

--- crediya/src/main/java/com/datavault/jdbc/PrestamoJdbcDv.java
+++ crediya/src/main/java/com/datavault/jdbc/PrestamoJdbcDv.java
@@ -1,5 +1,169 @@
 package com.datavault.jdbc;
 
-public class PrestamoJdbcDv {
-    
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.model.Prestamo;
+import com.datavault.PrestamoDv;
+import com.util.DBConnection;
+
+public class PrestamoJdbcDv implements PrestamoDv {
+  private final Connection conn;
+
+    public PrestamoJdbcDv() throws Exception {
+        this.conn = DBConnection.getConnection();
+    }
+
+    @Override
+    public Prestamo saveP(Prestamo prestamo) throws Exception {
+        String sql = "INSERT INTO prestamos (id, clienteId, empleadoId, monto, interes, saldo, cuotas, fechaInicio, estado) VALUES (?, ?, ?, ? , ? , ? , ? , ? , ? )";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, prestamo.getid());
+            ps.setInt(2, prestamo.getClienteId());
+            ps.setInt(3, prestamo.getEmpleadoId());
+            ps.setDouble(4, prestamo.getMonto());
+            ps.setDouble(5, prestamo.getInteres());
+            ps.setDouble(6, prestamo.getSaldo());
+            ps.setInt(7, prestamo.getCuotas());
+            ps.setDate(8, Date.valueOf(prestamo.getFechaInicio()));
+            
+            ps.executeUpdate();
+
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    prestamo.setId(rs.getInt(1));
+                }
+            }
+        }
+
+        String updatePrestamoSql =
+                "UPDATE prestamos SET saldo_pendiente = saldo_pendiente - ? WHERE id = ?";
+
+        try (PreparedStatement ps = conn.prepareStatement(updatePrestamoSql)) {
+            ps.setInt(1, prestamo.getid());
+            ps.setInt(2, prestamo.getClienteId());
+            ps.setInt(3, prestamo.getEmpleadoId());
+            ps.setDouble(4, prestamo.getMonto());
+            ps.setDouble(5, prestamo.getInteres());
+            ps.setDouble(6, prestamo.getSaldo());
+            ps.setInt(7, prestamo.getCuotas());
+            ps.executeUpdate();
+        }
+
+        return prestamo;
+    }
+
+    @Override
+    public Optional<Prestamo> findPById(int id) throws Exception {
+        String sql = "SELECT * FROM prestamos WHERE id = ?";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    Prestamo prestamo = new Prestamo();
+                    prestamo.setId(rs.getInt("id"));
+                    prestamo.setClienteId(rs.getInt("ClienteId"));
+                    prestamo.setEmpleadoId(rs.getInt("EmpleadoId"));
+                    prestamo.setMonto(rs.getDouble("Monto"));
+                    prestamo.setInteres(rs.getDouble("Interes"));
+                    prestamo.setSaldo(rs.getDouble("Saldo"));
+                    prestamo.setCuotas(rs.getInt("Cuotas"));
+                    Date fechaSQL = rs.getDate("fecha_pago");
+                    prestamo.setFechaInicio(fechaSQL != null ? fechaSQL.toLocalDate() : null);
+
+                    //pago.setMonto(rs.getDouble("monto"));
+                    return Optional.of(prestamo);
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Prestamo> findPAll() throws Exception {
+        String sql = "SELECT * FROM pagos";
+        List<Prestamo> list = new ArrayList<>();
+
+        try (Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Prestamo prestamo = new Prestamo();
+                prestamo.setId(rs.getInt("id"));
+                prestamo.setClienteId(rs.getInt("Cliente_id"));
+                prestamo.setEmpleadoId(rs.getInt("Empleado_id"));
+                prestamo.setMonto(rs.getDouble("monto"));
+                prestamo.setInteres(rs.getDouble("Interes"));
+                prestamo.setSaldo(rs.getDouble("Saldo"));
+                prestamo.setCuotas(rs.getInt("Cuotas"));
+                prestamo.setFechaInicio(rs.getDate("fecha_Inicio").toLocalDate());
+                prestamo.SetEstado(rs.getString("Estado"));
+                list.add(prestamo);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public List<Prestamo> findPByPrestamoId(int id) throws Exception {
+        String sql = "SELECT * FROM pretamos WHERE id = ?";
+        List<Prestamo> list = new ArrayList<>();
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                Prestamo prestamo = new Prestamo();
+                prestamo.setId(rs.getInt("id"));
+                prestamo.setClienteId(rs.getInt("Cliente_id"));
+                prestamo.setEmpleadoId(rs.getInt("Empleado_id"));
+                prestamo.setMonto(rs.getDouble("monto"));
+                prestamo.setInteres(rs.getDouble("Interes"));
+                prestamo.setSaldo(rs.getDouble("Saldo"));
+                prestamo.setCuotas(rs.getInt("Cuotas"));
+                prestamo.setFechaInicio(rs.getDate("fecha_Inicio").toLocalDate());
+                prestamo.SetEstado(rs.getString("Estado"));
+
+                list.add(prestamo);
+                }
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public void deletePById(int id) throws Exception {
+        String sql = "DELETE FROM prestamos WHERE id = ?";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+    }
+
+   @Override
+public void updateP(Prestamo prestamos) throws Exception {
+    String sql = "UPDATE prestamos SET cliente_id=?, empleado_id=?, fecha_inicio=?, monto=?, interes=?, saldo=?, cuotas=?, estado=? WHERE id=?";
+
+    try (PreparedStatement ps = conn.prepareStatement(sql)) {
+        ps.setInt(1, prestamos.getid());
+        ps.setInt(2, prestamos.getClienteId());
+        ps.setInt(3, prestamos.getEmpleadoId());
+        ps.setDouble(4, prestamos.getMonto());
+        ps.setDouble(5, prestamos.getInteres());
+        ps.setDouble(6, prestamos.getSaldo());
+        ps.setInt(7, prestamos.getCuotas());
+        ps.setDate(8, Date.valueOf(prestamos.getFechaInicio()));
+        ps.setString(9, prestamos.getEstado());
+        ps.executeUpdate();
+    }
+}
+
 }
